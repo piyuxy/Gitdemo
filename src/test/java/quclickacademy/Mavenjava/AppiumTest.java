b2eef64984b25445fe3e95a4348f59398ed10810
priya <piyuxy76@gmail.com>
2022-03-04 11:47:43 +0530

    first commit

---
+++ src/test/java/quclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,25 @@
+package quclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void Nativeappanroid()
+	{
+		System.out.println("Nativeappanroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+	
+	
+
+	
+	
+	
+	
+}
---
+++ src/test/java/quclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,31 @@
+package quclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deletTwitter()
+	{
+		System.out.println("deletTwitter");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
